57ead18247dcd4464dfd2271e7b1a60817d3dfd4
Will Friebel <will.friebel@gmail.com>
2016-07-26 14:54:26 -0700

    Add polymorphism

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,48 @@
-100.times do
+old_users = User.all
+old_users.each do |user|
+  user.destroy
+end
+
+old_questions = Comment.all
+old_questions.each do |question|
+  question.destroy
+end
+
+old_answers = Answer.all
+old_answers.each do |answer|
+  answer.destroy
+end
+
+old_comments = Comment.all
+old_comments.each do |comment|
+  comment.destroy
+end
+
+10.times do
   name = Faker::Internet.user_name
   password = "123"
   User.create!(username: name, password: password)
 end
 
-300.times do
+users = User.all
+
+50.times do
   title = Faker::Commerce.department(4)
   content = Faker::Company.catch_phrase + "?"
-  user_id = rand(1..100)
+  user_id = users.sample.id
   Question.create!(title: title, content: content, user_id: user_id)
 end
 
-1000.times do
+questions = Question.all
+
+150.times do
   content = Faker::Lorem.sentence(5)
-  question_id = rand(1..300)
-  user_id = rand(1..100)
+  question_id = questions.sample.id
+  user_id = users.sample.id
   Answer.create!(content: content, question_id: question_id, user_id: user_id)
+end
+
+300.times do
+  content = Faker::Lorem.sentence(5)
+  Comment.create!(content: content)
 end
